b8cc862dd207812fe622c2561f1a101f2266b965
swopnil7 <report2swopnil@gmail.com>
2025-02-12 21:52:04 +0545

    Initial Commit

---
+++ src/vector.h
@@ -0,0 +1,15 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+typedef struct {
+    double x;
+    double y;
+    double z;
+} Vector;
+
+void vector_add(Vector a, Vector b, Vector *result);
+void vector_subtract(Vector a, Vector b, Vector *result);
+double vector_dot_product(Vector a, Vector b);
+void vector_cross_product(Vector a, Vector b, Vector *result);
+
+#endif // VECTOR_H
